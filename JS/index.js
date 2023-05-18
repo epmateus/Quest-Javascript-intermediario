b53795e941b3bff0fe0af8092a4cb81443e36fe4
epmateus <mateus.eduardo.pereira00@gmail.com>
2023-05-18 14:15:07 -0300

    adicionado verificação dos inputs, alterado posicionamento das classes valido e observação

--- JS/index.js
+++ JS/index.js
@@ -5,12 +5,13 @@ const camposObrigatorios = document.querySelectorAll(".campo-obrigatorio")
 enviar.addEventListener("click", function(){
     infos.forEach(function(input, indice){
         if(input.value === ""){
+            input.classList.remove("valido");
             input.classList.add("invalido");
-            camposObrigatorios[indice].classList.add("campo-obrigatorio");
-            camposObrigatorios[indice].style.display = 'block';
+            camposObrigatorios[indice].style.display = "block";
         } else{
+            input.classList.remove("invalido");
             input.classList.add("valido");
-            camposObrigatorios[indice].classList.remove("campo-obrigatorio")
+            camposObrigatorios[indice].style.display = "none";
         }
     })
 })
