ababe9ef212809aff2ce9e2e5c9b4be74aab0dcf
epmateus <mateus.eduardo.pereira00@gmail.com>
2023-05-19 09:57:37 -0300

    adicionado onsubmit na tag form no html, adicionado preventDefault na funcao AutoRefresh

--- JS/index.js
+++ JS/index.js
@@ -1,17 +1,46 @@
-const enviar = document.getElementById("submit")
-const infos = document.querySelectorAll(".inputs")
-const camposObrigatorios = document.querySelectorAll(".campo-obrigatorio")
+// const enviar = document.getElementById("submit")
+// const infos = document.querySelectorAll(".inputs")
+// const camposObrigatorios = document.querySelectorAll(".campo-obrigatorio")
 
-enviar.addEventListener("click", function(){
-    infos.forEach(function(input, indice){
-        if(input.value === ""){
+// enviar.addEventListener("click", function(){
+//     infos.forEach(function(input, indice){
+//         if(input.value === ""){
+//             input.classList.remove("valido");
+//             input.classList.add("invalido");
+//             camposObrigatorios[indice].style.display = "block";
+//             class stopAutoRefresh{
+//                 adiciona(event){
+//                     event.preventDefault();
+//                 }
+//             }
+//         } else{
+//             input.classList.remove("invalido");
+//             input.classList.add("valido");
+//             camposObrigatorios[indice].style.display = "none";
+//             class stopAutoRefresh{
+//                 add(event){
+//                     event.preventDefault();
+//                 }
+//             }
+//         }
+//     })
+// })
+
+function stopAutoRefresh(event) {
+    event.preventDefault(); // Impede o comportamento padrão do formulário de atualizar a página
+
+    const infos = document.querySelectorAll(".inputs");
+    const camposObrigatorios = document.querySelectorAll(".campo-obrigatorio");
+    
+    infos.forEach(function (input, indice) {
+        if (input.value === "") {
             input.classList.remove("valido");
             input.classList.add("invalido");
             camposObrigatorios[indice].style.display = "block";
-        } else{
+        } else {
             input.classList.remove("invalido");
             input.classList.add("valido");
             camposObrigatorios[indice].style.display = "none";
         }
-    })
-})
+    });
+}
