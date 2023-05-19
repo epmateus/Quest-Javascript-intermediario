66282b00611d43cc5c47d655d664b7347c045aed
epmateus <mateus.eduardo.pereira00@gmail.com>
2023-05-19 10:05:16 -0300

    adicionado onsubmit na tag form no html, adicionado preventDefault na função stopAutoRefresh

--- JS/index.js
+++ JS/index.js
@@ -1,33 +1,5 @@
-// const enviar = document.getElementById("submit")
-// const infos = document.querySelectorAll(".inputs")
-// const camposObrigatorios = document.querySelectorAll(".campo-obrigatorio")
-
-// enviar.addEventListener("click", function(){
-//     infos.forEach(function(input, indice){
-//         if(input.value === ""){
-//             input.classList.remove("valido");
-//             input.classList.add("invalido");
-//             camposObrigatorios[indice].style.display = "block";
-//             class stopAutoRefresh{
-//                 adiciona(event){
-//                     event.preventDefault();
-//                 }
-//             }
-//         } else{
-//             input.classList.remove("invalido");
-//             input.classList.add("valido");
-//             camposObrigatorios[indice].style.display = "none";
-//             class stopAutoRefresh{
-//                 add(event){
-//                     event.preventDefault();
-//                 }
-//             }
-//         }
-//     })
-// })
-
 function stopAutoRefresh(event) {
-    event.preventDefault(); // Impede o comportamento padrão do formulário de atualizar a página
+    event.preventDefault();
 
     const infos = document.querySelectorAll(".inputs");
     const camposObrigatorios = document.querySelectorAll(".campo-obrigatorio");
